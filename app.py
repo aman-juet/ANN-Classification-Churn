e66f18ea3a5bb669644a2a5f84027d7f853a01f5
Aman Kumar <114340764+aman-juet@users.noreply.github.com>
2024-10-08 17:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ with open('label_encoder_gender.pkl', 'rb') as file:
 with open('Onehot_encoder_geo.pkl', 'rb') as file:
     Onehot_encoder_geo = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+with open('Scaler.pkl', 'rb') as file:
+    Scaler = pickle.load(file)
 
 
 ## streamlit app
